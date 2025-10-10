e4cd0dd173a0329f0a7df874a48cfb7e5b2c4bb5
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-10-10 06:37:39 +0530

    health api added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const indexRouter = require('express').Router();
 const awsRouter = require('./awsRouter.js');
 
+
+indexRouter.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Uttirna AWS Upload Service' });
+});
+
 indexRouter.use('/api/aws', awsRouter);
 
 module.exports = indexRouter;

aafd231550d834d374230c21c31979425f34afee
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2026-02-25 03:57:09 +0000

    ref

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,10 @@ module.exports = {
         {
             name: `aws-upload-service`,
             script: 'app.js',
-            exec_mode: 'cluster',
+            exec_mode: 'fork',
             instances: 1,
             autorestart: true,
-
-            max_memory_restart: '1G',
-
+	    max_memory_restart: '1G',
             watch: ['application/controllers', 'application/model', 'application/config', 'routes'],
             ignore_watch: [
                 'node_modules',
@@ -20,10 +18,8 @@ module.exports = {
             ],
 
             env: {
-                NODE_ENV: 'development',
             },
             env_production: {
-                NODE_ENV: 'production',
             },
         },
     ],
